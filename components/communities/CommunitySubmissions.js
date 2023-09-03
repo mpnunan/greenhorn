22478675f51dffb617318e5bde8d51099f92ef9a
Michael Nunan <michael.nunan@outlook.com>
2023-09-02 21:54:08 -0500

    bugfix

--- components/communities/CommunitySubmissions.js
+++ components/communities/CommunitySubmissions.js
@@ -1,23 +1,24 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { getCommunitySubmissions } from '../../api/communityData';
 import Submission from '../submissions/Submission';
 
 export default function CommunitySubmissions({ communityObj }) {
-  const [communitySubmissions, setCommunitySubmissions] = useState([]);
+  const [communitySubmissions, setCommunitySubmissions] = useState([communityObj]);
 
-  const communitySpecificSubmissions = (communityId) => {
-    getCommunitySubmissions(communityId).then(setCommunitySubmissions);
+  const communityPosts = () => {
+    getCommunitySubmissions(communityObj.id).then(setCommunitySubmissions);
   };
 
   useEffect(() => {
-    communitySpecificSubmissions(communityObj.id);
-  }, [communityObj]);
+    communityPosts();
+  }, []);
 
   return (
     <section className="communityPageSubmissions">
       {communitySubmissions.map((communitySubmission) => (
-        <Submission key={communitySubmission.id} submissionObj={communitySubmission} afterUpdate={communitySpecificSubmissions} />
+        <Submission key={communitySubmission.id} submissionObj={communitySubmission} afterUpdate={communityPosts} />
       ))}
     </section>
   );
