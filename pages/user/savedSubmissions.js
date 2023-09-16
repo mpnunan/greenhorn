556db38be2db8e61b72f6a1d258ae74d46b64bc8
Michael Nunan <michael.nunan@outlook.com>
2023-09-16 15:26:00 -0500

    added comments for discussion ticket

--- pages/user/savedSubmissions.js
+++ pages/user/savedSubmissions.js
@@ -1,40 +1,40 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
 import { Paper } from '@mui/material';
 import { useAuth } from '../../utils/context/authContext';
-import Submission from '../../components/submissions/Submission';
-import { getUserSaved } from '../../api/userSavedData';
-import { getSingleSubmission } from '../../api/submissionData';
+// import Submission from '../../components/submissions/Submission';
+import UserSavedSubmissions from '../../components/user/UserSavedSubmissions';
+// import { getUserSaved } from '../../api/userSavedData';
+// import { getSingleSubmission } from '../../api/submissionData';
 
 export default function SavedSubmissions() {
   const { user } = useAuth();
-  const [saved, setSaved] = useState([]);
-  const [savedSubmissions, setSavedSubmissions] = useState([]);
+  // const [submissions, setSubmissions] = useState([]);
+  // const [saved, setSaved] = useState([]);
 
-  const findSaveObjects = async (uid) => {
-    getUserSaved(uid).then(setSaved);
-  };
+  // const findSaveObjects = async () => {
+  //   getUserSaved(user.uid).then(setSaved);
+  // };
 
-  const findSavedSubmissions = (saveObjArray) => {
-    const saveArray = [];
-    saveObjArray.forEach((saveObj) => {
-      getSingleSubmission(saveObj.submissionId)
-        .then((submissionObj) => {
-          console.warn(submissionObj);
-          saveArray.push(submissionObj);
-        });
-    });
-    setSavedSubmissions(saveArray);
-    console.warn(savedSubmissions);
-  };
+  // const findSavedSubmissions = async (saveObjArray) => {
+  //   const saveArray = [];
+  //   await saveObjArray.forEach((saveObj) => {
+  //     getSingleSubmission(saveObj.submissionId)
+  //       .then((submissionObj) => {
+  //         saveArray.push(submissionObj);
+  //       });
+  //   });
+  //   return saveArray;
+  // };
 
-  useEffect(() => {
-    findSaveObjects(user.uid);
-  }, [user.uid]);
+  // useEffect(() => {
+  //   findSaveObjects();
+  // }, []);
 
-  useEffect(() => {
-    findSavedSubmissions(saved);
-  }, [saved]);
+  // useEffect(() => {
+  //   findSavedSubmissions(saved).then(setSubmissions);
+  // }, [saved]);
+  // console.warn(submissions);
 
   return (
     <Paper
@@ -49,11 +49,12 @@ export default function SavedSubmissions() {
           {`${user.displayName}'s Saved Submissions`}
         </h1>
       </header>
-      <section className="savedSection">
-        {savedSubmissions.map((savedSubmission) => (
-          <Submission key={savedSubmission.id} submissionObj={savedSubmission} afterUpdate={findSavedSubmissions(saved)} />
+      {/* <section className="savedSection">
+        {submissions.map((submission) => (
+          <Submission key={submission.id} submissionObj={submission} afterUpdate={findSaveObjects} />
         ))}
-      </section>
+      </section> */}
+      <UserSavedSubmissions />
     </Paper>
 
   );
